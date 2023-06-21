6072cecb5fb41c381219ed53176672ce40c51fba
Mohammed Salim <50304249+HazyAutumnRain@users.noreply.github.com>
2023-06-21 08:05:49 +0400

    Loading pokemon v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,45 @@
 import { Button } from '@chakra-ui/button'
 import Navigation from './Navigation'
-import { Box, Heading, Text } from '@chakra-ui/layout'
-import { useState } from 'react'
+import { Box, Container, Heading, Text } from '@chakra-ui/layout'
+import { useRef, useState } from 'react'
 import data from '../data.json'
+import { Image } from '@chakra-ui/image'
+import { Spinner } from '@chakra-ui/spinner'
+import { GiCheckeredFlag } from 'react-icons/gi'
+import { Icon } from '@chakra-ui/icon'
 
 function App() {
 
   const [headHover, setHeadHover] = useState(false);
+  const [pokeChosen, setPokeChosen] = useState(false);
+  const [loading, setLoading] = useState(false)
+  const pokeName = useRef()
+  const pokeImage = useRef()
+  //const p = new PokeAPI;
 
   function getPokemon() {
     // Choose a hunting method
     // If that method has a limited list, choose a pokemon from that list
     // Else choose a random pokemon
-    var encounter_opt = data.encounter_options;
-    var chosen_opt = encounter_opt[Math.floor(Math.random()*encounter_opt.length)];
-    switch(chosen_opt) {
-      case 'PokeRadar':
-        console.log("Radar has been chosen")
-        break;
-      case 'Random Encounters':
-        console.log("Random Encounter has been chosen")
-        break;
-    }
+
+    // REWORK
+    // choose a pokemon game
+    // Choose a pokemon from that game
+    // Show both
+    // For now, scarlet violet is only option
+    // More options to be added later
+
+    // REREWORK
+    // Randomly select a pokemon from the list
+    // Give timer for dialy reset and lock the user from rerolling
+    // Give counter and complete button
+    // On complete button 
+    setLoading(true)
+    var pokemon_chosen = data.names[Math.floor(Math.random() * data.names.length)];
+    pokeName.current.innerHTML = pokemon_chosen
+    pokeImage.current.src = 'https://play.pokemonshowdown.com/sprites/gen5-shiny/' + pokemon_chosen.toLowerCase() + '.png'
+
+    setPokeChosen(true)
   }
 
   return (
@@ -39,7 +57,16 @@ function App() {
           margin={8} 
           colorScheme='green'
           onClick={() => getPokemon()}
+          display={pokeChosen ? "none" : "inline-block"}
           >Start Hunt</Button>
+      </Box>
+      <Box display={loading ? "flex" : "none"} justifyContent={'center'} padding={4}>
+        <Spinner thickness='4px' speed='0.65s' size='xl' emptyColor='gray.200' color='green'/>
+      </Box>
+      <Box display={loading ? "none" : "flex"} flexDir='column' justifyContent={'center'} alignItems={'center'}>
+        <Image ref={pokeImage} marginBottom={-2} onLoad={() => setLoading(false)} w={36}/>
+        <Text ref={pokeName} fontSize='24'></Text>
+        <Button display={pokeChosen ? "block" : "none"} colorScheme='green' margin={2}><Icon as={GiCheckeredFlag} w={4} h={4}/></Button>
       </Box>
     </>
   )
